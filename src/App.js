a25d22484c87ea302906f096bb8404d9bd726a0e
Joshua Alexander <joshuaalexander@Joshuas-MacBook-Air.local>
2023-03-22 19:02:06 +0000

    initial set up code for react pages using the usestate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Home from './components/homepage/homepage'
+import Input from './components/inputpage/inputpage'
+import Navbar from "./components/navbar/navbar";
+import './index.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [currentPage, setCurrentPage] = useState("Home");
+
+    const handlePageChange = (page) => {
+      setCurrentPage(page);
+    };
+  
+    const renderPage = () => {
+      if (currentPage === "Home") {
+        return <Home />;
+      }  else {
+        return <Input />;
+      }
+    };
+  
+    return (
+      <div>
+        <Navbar
+          currentPage={currentPage}
+          handlePageChange={handlePageChange}
+        />
+        {renderPage()}
+      </div>
+    );
 }
 
 export default App;
---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function Navbar(props) {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <a
+          href="#home"
+          onClick={() => props.handlePageChange("Home")}
+          className={props.currentPage === "Home" ? "nav-link active" : "nav-link"}
+        >
+          Home
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#Input"
+          onClick={() => props.handlePageChange("Input")}
+          className={props.currentPage === "Input" ? "nav-link active" : "nav-link"}
+        >
+          About
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export default Navbar;
